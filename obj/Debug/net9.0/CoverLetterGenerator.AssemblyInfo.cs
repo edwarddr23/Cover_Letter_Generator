9d815cab1777deb1622fd5a1ee66fa0fc7f32f32
Edward Del Rosario <edwarddr23@gmail.com>
2025-09-27 17:36:58 -0400

    created published build under the bin directory for win-x64 self-contained .exe file

--- obj/Debug/net9.0/CoverLetterGenerator.AssemblyInfo.cs
+++ obj/Debug/net9.0/CoverLetterGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoverLetterGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8caef1212b56c885d9fe8f26c6e7368553457291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f8507080bef35adf736c93c8c29d264e744263")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoverLetterGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoverLetterGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
